9b45d02b13180b6c956f81fc1a63186f781c8d27
lragunath14 <lragunath14@gmail.com>
2021-09-18 13:46:18 +0530

    first commitISS

---
+++ TimePass_server/src/main/java/com/timepass/cricket/TimePassCricketTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.timepass.cricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimePassCricketTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimePassCricketTournamentApplication.class, args);
+	}
+
+}
